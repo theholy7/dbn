e4bd2338508ec23705720228a1521319bfe151f2
pedronobre <nobre11pedro@gmail.com>
2015-05-13 22:17:59 +0100

    introdução do calculo LL
    tenho praticamente as linhas todas explicadas

--- src/bayesNetwork/BayesNetwork.java
+++ src/bayesNetwork/BayesNetwork.java
@@ -71,6 +71,45 @@ public class BayesNetwork {
 		
 	}
 	
+	double private LLcalc(){
+		int numOcorren=0;
+		int nijk=0;
+		int nij=0;
+		int Nij=0;
+		double Nijk=0; 
+		double sumNijk=0; // uso double ou float ?
+		
+		for(Node n: this.nodeList){// percorre os nós
+			for (Edge e: this.edgeList){//percorre os edges
+				if(e.parentNode == n){
+					int baseParent = n.dataType; // ficar a saber a quantidade de valores que xn toma
+					int baseChild= e.childNode.dataType;
+					for(int g=0; g<baseParent; g++){//percorre todos os valores possíveis para xn
+						for(int j=0; j<=n.data.length;j++){//percorre vector de valores
+							if(n.data[j]==g){// se encontrar valor no vector pai
+								for(int h=0;h<baseChild;h++){ //percorre todos os valores possiveis do filho
+									Nij=Nij+nij;
+									for(int t=0;t<=e.childNode.data.length;t++){
+										if(e.childNode.data[t]==h){
+											nijk++;
+											nij++;
+											if(t==e.childNode.data.length){//ja viu todas as vezes que o filho toma determinado valor para determinado valor de pai
+												sumNijk=sumNijk+Nijk*(Math.log(Nijk/Nij)/Math.log(2));
+											}
+										}
+									}	
+								}
+							}
+						}
+						
+					}
+				}
+				
+			}
+		}
+		return sumNijk;
+	}
+	
 	boolean isDag(){
 		//Check if network is DAG
 		return false;	
